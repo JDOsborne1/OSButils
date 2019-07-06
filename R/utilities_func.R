cb3339fb24b7b3d862b6af171ec8c81662c1a03b
Joseph Osborne <j_osborne@edit.co.uk>
2019-07-06 18:00:13 +0100

    added onestop data transform

--- R/utilities_func.R
+++ R/utilities_func.R
@@ -30,3 +30,28 @@ printStepTitle <- function(string){
 getPropVect <- function(vect){
   return(vect/sum(vect))
 }
+
+#' One Stop Data Type Formatting
+#'
+#' A function to perform the routine data type transformations at the beggining of a data flow in one easy go.
+#' Most data is read into R as character data, but most useful data we work with is not character data,
+#' this package allows you to direct R to transform all the columns suiting a regular expression you define to
+#' be transformed as either date, numeric or factor data.
+#'
+#' @param dfin The input dataframe
+#' @param dateform The format of any dates in the dataframe as a string
+#' @param datereg The regular expression to determine if a column name justifies treating it as a date
+#' @param numreg The regular expression to determine if a column name justifies treating it as numeric
+#' @param flagreg The regular expression to determine if a column name justifies treating it as a factor
+#'
+#' @return The dataframe with the appropriate data type transformations applied to the columns
+#' @export
+#'
+#' @examples
+data_type_format <- function(dfin, dateform = "%Y-%m-%d", datereg = "date", numreg = "value", flagreg = "flag") {
+  dfout <- dfin %>%
+    mutate_at(vars(matches(datereg)),funs(as.Date(as.character(.),format=dateform)))%>%
+    mutate_at(vars(matches(numreg)),funs(as.numeric))%>%
+    mutate_at(vars(matches(flagreg)),funs(as.factor))
+  return(dfout)
+}
