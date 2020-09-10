c4dc23d6693b6b9c3c0137d60d41e06be683c2cd
JDO <atharos@vivaldi.net>
2020-09-10 19:27:16 +0100

    Updating the namespace function with some error handling and improved documentation

--- R/namespace_utils.R
+++ R/namespace_utils.R
@@ -1,7 +1,10 @@
 #' Get the currently used namespace
 #'
 #' @description Function to get the used namespace and prepend it to the
-#'   function name
+#'   function name.
+#'
+#'   When it is used on a function with no defintion, or a function which is a
+#'   part of the base packages, it will simply return the input unchanged.
 #'
 #' @param .function_name
 #'
@@ -10,7 +13,15 @@
 #'
 #' @examples
 get_used_namespace <- function(.function_name){
-  paste0(gsub("^.*:", "", attr(findFunction(.function_name)[[1]], "name")), "::", .function_name)
+  if (length(findFunction(.function_name)) == 0){
+    print("Function is not currently defined anywhere")
+    return(.function_name)
+  } else if (is.null(attr(findFunction(.function_name)[[1]], "name"))){
+    print("Function environment has no name, likely a base function")
+    return(.function_name)
+  } else {
+    paste0(gsub("^.*:", "", attr(findFunction(.function_name)[[1]], "name")), "::", .function_name)
+  }
 }
 
 
@@ -36,4 +47,5 @@ prepend_namespace <- function(){
     # replaces selection with string
     rstudioapi::modifyRange(ctx$selection[[1]]$range, selected_text)
   }
+
 }
