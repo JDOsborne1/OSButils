26efbe551d2ecd48480737d0c82aedc8adbe89fa
User <user@Users-MacBook-Air.local>
2020-11-20 18:11:08 +0000

    Adding in documentation for the index caster function

--- R/index_caster.R
+++ R/index_caster.R
@@ -1,3 +1,16 @@
+#' Index Caster Function
+#'
+#' @description A function to cast an inde as an measure of its deviation from a
+#' standard, circumventing the issue of fractional changes in negative indicies.
+#'
+#' @param .index The supplied index value to be converted
+#'
+#' @export
+#' @examples
+#'      util_IndexCaster(50)
+#'
+#'      util_IndexCaster(200)
+#'
 util_IndexCaster <- function(.index){
   if(.index >= 1){
     output <- .index - 1
